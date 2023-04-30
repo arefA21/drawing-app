2f9f0b231fdd941ce07cdbf77462fcdcc8c58259
aref <arefabolhasani21@gmail.com>
2023-04-30 23:06:17 +0330

    adding files

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useRef, useState } from "react";
+
+function App() {
+  const canvasRef = useRef(null);
+  const contextRef = useRef(null);
+  const [isDrawing, setIsDrawing] = useState(false);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.width = window.innerWidth * 2;
+    canvas.height = window.innerHeight * 2;
+    canvas.style.width = `${window.innerWidth}px`;
+    canvas.style.height = `${window.innerHeight}px`;
+
+    const context = canvas.getContext("2d");
+    context.scale(2, 2);
+    context.lineCap = "round";
+    contextRef.current = context;
+  }, []);
+
+  const startPainting = ({ nativeEvent }) => {
+    const { offsetX, offsetY } = nativeEvent;
+    contextRef.current.beginPath();
+    contextRef.current.moveTo(offsetX, offsetY);
+    setIsDrawing(true);
+  };
+
+  const finishPainting = () => {
+    contextRef.current.closePath();
+  };
+
+  const painting = ({ nativeEvent }) => {
+    if (!isDrawing) {
+      return null;
+    }
+    const { offsetX, offsetY } = nativeEvent;
+    contextRef.current.lineTo(offsetX, offsetY);
+    contextRef.current.stroke();
+  };
+
+  return (
+    <canvas
+      ref={canvasRef}
+      onMouseDown={startPainting}
+      onMouseUp={finishPainting}
+      onMouseMove={painting}
+    />
+  );
+}
+
+export default App;
